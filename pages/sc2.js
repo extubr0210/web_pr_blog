b092abddc31e8b98f01f0b64d01fa07b185b8119
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2022-01-19 12:44:21 +0300

    Add files via upload

---
+++ pages/sc2.js
@@ -0,0 +1,6 @@
+function addel(){
+var div1=document.createElement("div");
+var text1 = document.createTextNode("Some Text");
+div1.appendChild(text1);
+document.getElementById("d1").appendChild(div1);
+}
---
+++ script1.js
@@ -0,0 +1,26 @@
+
+let ll1=new Date()
+
+
+function cC(){
+let bl1=document.querySelector('#bl11');
+bl1.style.color='white';
+}
+
+
+function f1(){let ll2=new Date();let ll3=(ll2-ll1);
+//alert('страница загрузилась за '+ll3+' ms')
+}
+
+function moov(){
+this.style.color='green'}
+
+function moou(){
+this.style.color='gray'}
+
+function addel(){alert('et');
+}
+
+//let pr01 = new Promise((resolve,reject)=>{
+//setTimeout(()=>{resolve("some");},300);});
+//pr01.then(result=>{alert("1")},error=>{alert("2")});
